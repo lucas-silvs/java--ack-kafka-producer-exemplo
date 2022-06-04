e2fa6dddd9630d96d0dad2d779d65d7852e7e755
Lucas Santos <lucas.parkl@outlook.com>
2022-06-04 18:55:17 -0300

    removendo campos não utilizados para postar no tópico

--- kafka-acknowledgement/src/main/java/com/ack/kafkaacknowledgement/models/request/MensagemRequest.java
+++ kafka-acknowledgement/src/main/java/com/ack/kafkaacknowledgement/models/request/MensagemRequest.java
@@ -6,10 +6,6 @@ import org.springframework.lang.NonNull;
 @Data
 public class MensagemRequest {
     @NonNull
-    private String origem;
-    @NonNull
-    private String data;
-    @NonNull
     private String mensagem;
     @NonNull
     private String topico;
