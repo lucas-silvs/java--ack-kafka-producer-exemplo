b63933a6f4a7360256dd19026c3b00c9f239e93c
Lucas Santos <lucas.parkl@outlook.com>
2022-06-04 19:02:02 -0300

    tratamndo lançamento de excessões gerais com um response padrão que retorna qual erro foi lançado

--- kafka-acknowledgement/src/main/java/com/ack/kafkaacknowledgement/controllers/impl/MensagemImpl.java
+++ kafka-acknowledgement/src/main/java/com/ack/kafkaacknowledgement/controllers/impl/MensagemImpl.java
@@ -4,6 +4,7 @@ import com.ack.kafkaacknowledgement.controllers.Mensagem;
 import com.ack.kafkaacknowledgement.models.request.MensagemRequest;
 import com.ack.kafkaacknowledgement.models.response.MensagemResponse;
 import com.ack.kafkaacknowledgement.service.mensagem.MensagemService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,17 +12,24 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/kafka")
 public class MensagemImpl implements Mensagem {
 
+    @Autowired
     private MensagemService mensagemService;
 
     @PostMapping("/postar-mensagem")
     public ResponseEntity<MensagemResponse> postaMensagemNoTopico(@RequestBody MensagemRequest mensagem) {
-       MensagemResponse response = mensagemService.postarMensagem(mensagem);
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        try {
+            MensagemResponse response = mensagemService.postarMensagem(mensagem);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }catch (Exception e){
+            return  new ResponseEntity<>(MensagemResponse.builder().
+                    mensagemStatusPostado("Ocorreu algum erro ao publicar mensagem: error - "+e.getMessage())
+                    .build(), HttpStatus.UNPROCESSABLE_ENTITY);
+        }
     }
 }
