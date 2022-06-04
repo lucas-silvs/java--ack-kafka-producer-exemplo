835c83b16d1e59a7b0eb354e63d8c367ce2dda8b
Lucas Santos <lucas.parkl@outlook.com>
2022-06-04 19:04:48 -0300

    adicionado um try catch ao tentar enviar a mensagem, aqui caso não consiga é lançado no log e o fluxo normal do teste segue

--- kafka-acknowledgement/src/main/java/com/ack/kafkaacknowledgement/gateway/kafka/KafkaPost.java
+++ kafka-acknowledgement/src/main/java/com/ack/kafkaacknowledgement/gateway/kafka/KafkaPost.java
@@ -2,15 +2,19 @@ package com.ack.kafkaacknowledgement.gateway.kafka;
 
 import lombok.extern.log4j.Log4j;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.errors.TimeoutException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Component
 public class KafkaPost {
 
-    private KafkaTemplate<String, String> kafkaTemplate;
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
     @Autowired
     KafkaPost(KafkaTemplate<String, String> kafkaTemplate) {
@@ -18,8 +22,18 @@ public class KafkaPost {
     }
 
     public void sendMessage(String message, String topicName) {
-        kafkaTemplate.send(topicName, message);
-        log.info("Mensagem postada no topico:  "+topicName);
+        try {
+            kafkaTemplate.send(topicName, message).get(3, TimeUnit.SECONDS);
+            log.info("Mensagem postada no topico:  " + topicName);
+        } catch (ExecutionException e) {
+            log.warn("Erro ao enviar mensagem para o tópico, execução");
+        } catch (InterruptedException e) {
+            log.warn("Erro ao enviar mensagem para o tópico, interrompido");
+            Thread.currentThread().interrupt();
+        } catch (java.util.concurrent.TimeoutException e) {
+            log.warn("Erro ao enviar mensagem para o tópico, timeout");
+
+        }
     }
 }
 
