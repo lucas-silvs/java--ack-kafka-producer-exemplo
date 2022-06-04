8318089ca90013572486b15400ebe0616d28a4c6
Lucas Santos <lucas.parkl@outlook.com>
2022-06-03 21:35:02 -0300

    envio do main spring boot

---
+++ kafka-acknowledgement/src/main/java/com/ack/kafkaacknowledgement/KafkaAcknowledgementApplication.java
@@ -0,0 +1,13 @@
+package com.ack.kafkaacknowledgement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaAcknowledgementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaAcknowledgementApplication.class, args);
+    }
+
+}
